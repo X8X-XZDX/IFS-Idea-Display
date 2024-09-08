1afb5d129cb9921ecc2d60a0487a49c5b9cc71bb
Garrett Gunnell <gunnellg@oregonstate.edu>
2024-09-07 18:54:43 -0700

    Debug individual transformations

--- Assets/DebugTransform.cs
+++ Assets/DebugTransform.cs
@@ -7,11 +7,22 @@ public class DebugTransform : MonoBehaviour {
     public Shader particleShader;
     public ComputeShader particleUpdater;
 
+    public int debugIndex = 0;
+
+    [Range(0, 1)]
+    public float t = 0;
+
+    public bool animate = false;
+    [Range(0, 10)]
+    public float speed = 1.0f;
+
+    private float bounce = 1;
+
     private Material particleMaterial;
 
     private RenderParams renderParams;
 
-    private GraphicsBuffer commandBuffer, positionBuffer;
+    private GraphicsBuffer commandBuffer, positionBuffer, destinationBuffer;
     private GraphicsBuffer.IndirectDrawIndexedArgs[] commandIndexedData;
     private ComputeBuffer attractorsBuffer;
     
@@ -21,12 +32,13 @@ public class DebugTransform : MonoBehaviour {
         affineTransformations = GetComponent<AffineTransformations>();
         particleMaterial = new Material(particleShader);
 
-        positionBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 32768, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector4)));
+        positionBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 512, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector4)));
+        destinationBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, 512, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Vector4)));
 
         
         commandBuffer = new GraphicsBuffer(GraphicsBuffer.Target.IndirectArguments, 1, GraphicsBuffer.IndirectDrawIndexedArgs.size);
         commandIndexedData = new GraphicsBuffer.IndirectDrawIndexedArgs[1];
-        commandIndexedData[0].instanceCount = 32768;
+        commandIndexedData[0].instanceCount = 512;
         commandIndexedData[0].indexCountPerInstance = particleMesh.GetIndexCount(0);
 
         commandBuffer.SetData(commandIndexedData);
@@ -35,18 +47,51 @@ public class DebugTransform : MonoBehaviour {
         renderParams.worldBounds = new Bounds(Vector3.zero, 10000 * Vector3.one);
         renderParams.matProps = new MaterialPropertyBlock();
 
-        renderParams.matProps.SetBuffer("_Positions", positionBuffer);
+        renderParams.matProps.SetBuffer("_Origins", positionBuffer);
+        renderParams.matProps.SetBuffer("_Destinations", destinationBuffer);
 
+        particleUpdater.SetInt("_CubeResolution", 8);
+        particleUpdater.SetFloat("_CubeSize", 1.0f / 8.0f);
         particleUpdater.SetBuffer(0, "_PositionBuffer", positionBuffer);
-        particleUpdater.Dispatch(0, Mathf.CeilToInt(32768.0f / 8.0f), 1, 1);
+        particleUpdater.Dispatch(0, Mathf.CeilToInt(512.0f / 8.0f), 1, 1);
+        particleUpdater.SetBuffer(0, "_PositionBuffer", destinationBuffer);
+        particleUpdater.Dispatch(0, Mathf.CeilToInt(512.0f / 8.0f), 1, 1);
         
         attractorsBuffer = new ComputeBuffer(32, System.Runtime.InteropServices.Marshal.SizeOf(typeof(Matrix4x4)));
     }
 
     void Update() {
-        particleUpdater.SetInt("_TransformationCount", affineTransformations.GetTransformCount());
-
+        
         attractorsBuffer.SetData(affineTransformations.GetTransformData());
+
+        if (debugIndex > (affineTransformations.GetTransformCount() - 1))
+            debugIndex = affineTransformations.GetTransformCount() - 1;
+
+        particleUpdater.SetInt("_DebugIndex", debugIndex);
+
+        // Reset Particles To Origin
+        particleUpdater.SetBuffer(0, "_PositionBuffer", destinationBuffer);
+        particleUpdater.Dispatch(0, Mathf.CeilToInt(512.0f / 8.0f), 1, 1);
+
+        // Apply Debug Transform
+        particleUpdater.SetBuffer(5, "_Transformations", attractorsBuffer);
+        particleUpdater.SetBuffer(5, "_PositionBuffer", destinationBuffer);
+        particleUpdater.Dispatch(5, Mathf.CeilToInt(512.0f / 8.0f), 1, 1);
+
+        
+        if (animate) {
+            t += Time.deltaTime * bounce * speed;
+            if (t > 1) {
+                t = 1;
+                bounce = -1;
+            } else if (t < 0) {
+                t = 0;
+                bounce = 1;
+            }
+        }
+
+        renderParams.matProps.SetFloat("_Interpolator", t);
+
 
         Graphics.RenderMeshIndirect(renderParams, particleMesh, commandBuffer, 1);
     }
@@ -55,6 +100,7 @@ public class DebugTransform : MonoBehaviour {
         commandBuffer.Release();
         attractorsBuffer.Release();
         positionBuffer.Release();
+        destinationBuffer.Release();
 
         commandBuffer = null;
         attractorsBuffer = null;
